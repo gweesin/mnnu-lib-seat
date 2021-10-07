6c2b15b662408f90f8c8ca3aef67fa3daaa1d0eb
guixinchan <chan249@foxmail.com>
2021-10-07 09:46:08 +0800

    完善cycle-timer测试

--- test/cycle-timer-test.ts
+++ test/cycle-timer-test.ts
@@ -1,8 +1,13 @@
-import moment, { Moment } from "_moment@2.29.1@moment";
+import moment, { Moment } from "moment";
 import CycleTimer from "../src/entity/cycle-timer";
 
 const todayMoment: Moment = moment();
 
-new CycleTimer(() => {
-  console.log("等待成功！" + moment().format("YYYY-MM-DD HH:mm:ss"));
+new CycleTimer(async () => {
+  await new Promise(() => {
+    console.log("开始等待！" + moment().format("YYYY-MM-DD HH:mm:ss"));
+    const twoSeconds = new Date().setSeconds(new Date().getSeconds() + 15);
+    while (Date.now() < twoSeconds);
+    console.log("等待成功！" + moment().format("YYYY-MM-DD HH:mm:ss"));
+  });
 }, moment().add(2, "seconds")).start();
